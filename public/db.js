ec970e466776a6a18bdcf1eb97334cbd00e9f54b
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-12 15:52:54 -0400

    Finished db.js and service-worker.js

--- public/db.js
+++ public/db.js
@@ -1,53 +1,59 @@
 let db;
-const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function (event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+// New request for a Budget Tracker database
+const request = indexedDB.open("Budget_Tracker", 1);
+
+request.onupgradeneeded = function (e) {
+  console.log("Upgrade needed!!!");
+  db = e.target.result;
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore("BudgetTracker", { autoIncrement: true });
+  }
 };
 
-request.onsuccess = function (event) {
-  db = event.target.result;
-
-  if (navigator.onLine) {
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+  if (navigator.online) {
+    console.log("Success!!!");
     checkDatabase();
   }
 };
 
-request.onerror = function (event) {
-  console.log("This isn't working!" + event.target.errorCode);
+request.onerror = function (e) {
+  console.log("Broken!!!" + e.target.errorCode);
 };
 
-function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
-
-  const store = transaction.objectStore("pending");
+// Adding/creating to budget tracker
+const saveRecord = (record) => {
+  const transaction = db.transaction(["BudgetTracker"], "readwrite");
+  const store = transaction.objectStore("BudgetTracker");
   store.add(record);
-}
+};
 
+// Pulling existing entries
 function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-
-  const store = transaction.objectStore("pending");
-
+  const transaction = db.transaction(["BudgetTracker"], "readwrite");
+  const store = transaction.objectStore("BudgetTracker");
   const getAll = store.getAll();
-
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: "application/json, textplain, */*",
+          Accept: "application/json, text/plain, */*",
           "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
-        .then(() => {
-          const transaction = db.transaction(["pending"], "readwrite");
-          const store = transaction.objectStore("pending");
-
-          store.clear();
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetTracker"], "readwrite");
+            const currentStore = transaction.objectStore("BudgetTracker");
+            currentStore.clear();
+            console.log("Cleared!!!");
+          }
         });
     }
   };
