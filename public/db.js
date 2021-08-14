76cbed54c903083a80c0e0bd53da6795fbbddc63
Pratik Patel <pratikpatel_85@yahoo.com>
2021-08-14 15:31:38 -0400

    Renaming a few constants

--- public/db.js
+++ public/db.js
@@ -1,62 +1,67 @@
 let db;
+let budgetVersion;
 
-// New request for a Budget Tracker database
-const request = indexedDB.open("Budget_Tracker", 1);
+// New Request for a Budget Tracker database
+const request = indexedDB.open("budgetTracker", budgetVersion || 10);
 
 request.onupgradeneeded = function (e) {
-  console.log("Upgrade needed!!!");
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
   db = e.target.result;
   if (db.objectStoreNames.length === 0) {
-    db.createObjectStore("BudgetTracker", { autoIncrement: true });
+    db.createObjectStore("transactions", { autoIncrement: true });
   }
 };
 
+// Success
 request.onsuccess = function (e) {
-  console.log("success");
+  console.log("Request Successful!!!");
   db = e.target.result;
-  if (navigator.online) {
-    console.log("Success!!!");
+  if (navigator.onLine) {
+    console.log("Request Successful!!!");
     checkDatabase();
   }
 };
 
+// Failure
 request.onerror = function (e) {
-  console.log("Broken!!!" + e.target.errorCode);
+  console.log(`Error!!! + ${e.target.errorCode}`);
 };
 
-// Adding/creating to budget tracker
-const saveRecord = (record) => {
-  const transaction = db.transaction(["BudgetTracker"], "readwrite");
-  const store = transaction.objectStore("BudgetTracker");
-  store.add(record);
-};
-
-// Pulling existing entries
+// Pulls all existing entries
 function checkDatabase() {
-  const transaction = db.transaction(["BudgetTracker"], "readwrite");
-  const store = transaction.objectStore("BudgetTracker");
+  const transaction = db.transaction(["transactions"], "readwrite");
+  const store = transaction.objectStore("transactions");
   const getAll = store.getAll();
   getAll.onsuccess = function () {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((res) => {
-          if (res.length !== 0) {
-            transaction = db.transaction(["BudgetTracker"], "readwrite");
-            const currentStore = transaction.objectStore("BudgetTracker");
-            currentStore.clear();
-            console.log("Cleared!!!");
-          }
-        });
-    }
+    fetch("/api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        if (res.length !== 0) {
+          transaction = db.transaction(["transactions"], "readwrite");
+          const currentStore = transaction.objectStore("transactions");
+          currentStore.clear();
+          console.log("Clearing store 🧹");
+        }
+      });
   };
+  caches.keys().then(function (names) {
+    for (let name of names) caches.delete(name);
+  });
 }
+
+// Adding/Creating to budget tracker
+const saveRecord = (record) => {
+  const transaction = db.transaction(["transactions"], "readwrite");
+  const store = transaction.objectStore("transactions");
+  store.add(record);
+};
 
 window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -10,63 +10,35 @@ const FILES_TO_CACHE = [
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "precache-v10";
+const RUNTIME = "runtime";
 
-self.addEventListener("install", function (evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Files were pre-cached!!!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
-
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removed old cache data!!!", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-              return response;
-            })
-            .catch((err) => {
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-    return;
-  }
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
+self.addEventListener("activate", (event) => {
+  const currentCaches = [CACHE_NAME, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
